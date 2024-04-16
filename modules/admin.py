dff1697509189c19a2fa13abace7ad14db94094e
NeoMahler <roc.humet@gmail.com>
2024-04-16 15:03:53 +0900

    remove guild-specific command

--- modules/admin.py
+++ modules/admin.py
@@ -18,7 +18,7 @@ class AdminCog(commands.Cog):
         output = subprocess.check_output("git pull", shell=True)
         await ctx.respond("Pulling latest version from GitHub: ```" + str(output) + "``` Remember to use the reload command for the changes to take effect.")
 
-    @slash_command(name="load", description="Loads a previously unloaded module (owner-only).", guild_ids=[551837071703146506])
+    @slash_command(name="load", description="Loads a previously unloaded module (owner-only).")
     @commands.is_owner()
     async def load(self, ctx, cog: Option(str, "Name of the module (without .py extension)")):
         module = "modules." + cog
@@ -29,7 +29,7 @@ class AdminCog(commands.Cog):
         else:
             await ctx.respond('Module loaded! :tada:')
 
-    @slash_command(name="unload", description="Unloads a previously loaded module (owner-only).", guild_ids=[551837071703146506])
+    @slash_command(name="unload", description="Unloads a previously loaded module (owner-only).")
     @commands.is_owner()
     async def unload(self, ctx, cog: Option(str, "Name of the module (without .py extension)")):
         module = "modules." + cog
@@ -40,7 +40,7 @@ class AdminCog(commands.Cog):
         else:
             await ctx.respond('Module unloaded! :tada:')
 
-    @slash_command(name="reload", description="Reloads a previously loaded module (owner-only).", guild_ids=[551837071703146506])
+    @slash_command(name="reload", description="Reloads a previously loaded module (owner-only).")
     @commands.is_owner()
     async def reload(self, ctx, cog: Option(str, "Name of the module (without .py extension)")):
         module = "modules." + cog
