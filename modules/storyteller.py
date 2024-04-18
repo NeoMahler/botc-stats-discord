dd89acd9a5903ecb591626832899504d386237ac
NeoMahler <roc.humet@gmail.com>
2024-04-18 15:35:51 +0900

    Basic boomdandy implementation

--- modules/storyteller.py
+++ modules/storyteller.py
@@ -11,28 +11,34 @@ class StorytellerCog(commands.Cog):
     @slash_command(name="boomdandy", description="Boom!", guild_ids=[551837071703146506])
     async def boomdandy(self, ctx, players: Option(str, "Space-separated list of surviving players (using Discord mentions).")):
         clean_players = []
+        player_members = []
         for player in players.split(" "):
             player = self.utilities.clean_id(player) # Remove the <@> part of the user ID
             if not player:
                 await ctx.respond(f"El usuario {player} no es válido. Solo acepto menciones o IDs de usuarios.")
                 return
             clean_players.append(str(player))
+            player_member = await self.bot.fetch_user(int(player))
+            player_members.append(player_member)
         
+        pings = [f'<@{player}>' for player in clean_players]
         msg = "¡El Boomdandy ha explotado! Los jugadores que sobrevivieron son:\n"
-        msg += f"{' '.join(clean_players)}\n\n"
+        msg += f"{' '.join(pings)}\n\n"
         msg += "Tenéis **1 minuto** para decidir quien va a morir. Usad los botones para escogerlo."
 
-        await ctx.respond(msg, view=self.BoomdandyUI(ctx, clean_players))
+        await ctx.respond(msg, view=self.BoomdandyUI(ctx, player_members))
         return
 
     class BoomButton(discord.ui.Button):
         def __init__(self, ctx, player):
-            super().__init__(label=str(player), style=discord.ButtonStyle.primary)
+            super().__init__(label=player.display_name, style=discord.ButtonStyle.primary)
             self.ctx = ctx
             self.disabled = False
+            self.player = player
         
         async def callback(self, interaction):
-            await self.ctx.send(f"Apuntando a {self.label}")
+            await interaction.response.defer()
+            await self.ctx.send(f"<@{interaction.user.id}> está apuntando a <@{self.player.id}>")
             return
 
     class BoomdandyUI(discord.ui.View):
